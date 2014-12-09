8895bdcc7fe7a8966e177663448ce26846dbfcbf
JB CADOT <jean-baptiste.cadot@openwide.fr>
2014-12-09 10:54:05 +0100

    tests

--- Jiboule/DDumpBundle/JibouleDDumpBundle.php
+++ Jiboule/DDumpBundle/JibouleDDumpBundle.php
@@ -7,15 +7,3 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class JibouleDDumpBundle extends Bundle
 {
 }
-
-if (!function_exists('dd') && !function_exists('d')) {
-    function dd ($mixed, $params = array())
-    {
-        d($mixed, $params);
-        exit();
-    }
-    function d ($mixed, $params = array())
-    {
-        echo '<pre class="dd"'.(isset($params['style']) ? ' style="'.$params['style'].'"' : '').'>'.print_r($txt, true).'</pre>';
-    }
-}
---
+++ Jiboule/DDumpBundle/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+echo 'c';
+exit();
+
+if (!function_exists('dd') && !function_exists('d')) {
+    function dd ($mixed, $params = array())
+    {
+        d($mixed, $params);
+        exit();
+    }
+    function d ($mixed, $params = array())
+    {
+        echo '<pre class="dd"'.(isset($params['style']) ? ' style="'.$params['style'].'"' : '').'>'.print_r($txt, true).'</pre>';
+    }
+}
