af4297779da9ed33c81d737856c2f6b3cee413e3
jiboule <=>
2014-12-09 11:49:09 +0100

    functions

--- Jiboule/DDumpBundle/functions/functions.php
+++ Jiboule/DDumpBundle/functions/functions.php
@@ -1,18 +1,19 @@
 <?php
 
 if (!function_exists('dd') && !function_exists('d')) {
-    function dd ($mixed, $params = array())
+    function dd($mixed, $params = array())
     {
         d($mixed, $params);
         exit();
     }
-    function d ($mixed, $params = array())
+
+    function d($mixed, $params = array())
     {
         echo '<pre class="dd"'.(isset($params['style']) ? ' style="'.$params['style'].'"' : '').'>'.print_r($mixed, true).'</pre>';
     }
 }
 
-if(!function_exists('ddump')) {
+if (!function_exists('ddump')) {
     function ddump($var)
     {
         dump($var);
