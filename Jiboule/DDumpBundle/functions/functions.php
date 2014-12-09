bc55b03ebabe9fcc4536263874ac2a0e41c991c0
jiboule <jbcadot@hotmail.com>
2014-12-09 11:53:30 +0100

    functions

--- Jiboule/DDumpBundle/functions/functions.php
+++ Jiboule/DDumpBundle/functions/functions.php
@@ -1,12 +1,20 @@
 <?php
 
 if (!function_exists('dd') && !function_exists('d')) {
+    /**
+     * @param $mixed
+     * @param array $params
+     */
     function dd($mixed, $params = array())
     {
         d($mixed, $params);
         exit();
     }
 
+    /**
+     * @param $mixed
+     * @param array $params
+     */
     function d($mixed, $params = array())
     {
         echo '<pre class="dd"'.(isset($params['style']) ? ' style="'.$params['style'].'"' : '').'>'.print_r($mixed, true).'</pre>';
@@ -14,6 +22,9 @@ if (!function_exists('dd') && !function_exists('d')) {
 }
 
 if (!function_exists('ddump')) {
+    /**
+     * @param $var
+     */
     function ddump($var)
     {
         dump($var);
